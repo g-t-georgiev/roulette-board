2d65b41e9aabd7aacb77ed44cc0b3f5c0bd3290f
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-03-27 21:01:50 +0300

    Added base skeleton for the slot component.

---
+++ app/board/slot/slot.js
@@ -0,0 +1,24 @@
+import BetManager from '../../services/bet-manager.js';
+
+export class RouletteSlot extends HTMLElement {
+
+    constructor() {
+        super();
+        this.rendered = false;
+    }
+
+    connectedCallback() {
+
+        if (!this.rendered) {
+            this.rendered = true;
+            console.log('Slot component was rendered!');
+            BetManager.register(this);
+        }
+
+    }
+
+    disconnectedCallback() {
+        console.log('Slot component was removed!');
+    }
+
+}
