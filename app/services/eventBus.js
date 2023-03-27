931a4f430d850ac0ccb2c5564f9066dde878ef42
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-03-27 19:21:41 +0300

    Refactored eventBus service into a module exporting publish/subscribe functionality.

--- app/services/eventBus.js
+++ app/services/eventBus.js
@@ -1,5 +1,63 @@
-import { PubSub } from '../../utils/PubSub.js';
+// import { PubSub } from '../../utils/PubSub.js';
 
-const evntBus = new PubSub();
+/**
+ * @typedef Subscriptions
+ * @property {{ [id: symbol]: (...args: any[]) => void }} [eventType]
+ */
 
-export default evntBus;
+/**
+ * List of subscriptions where each entry is an event type with
+ * subscribers (callback functions) registered with an id under that event. 
+ * @type Subscriptions
+ */
+const subscriptions = {};
+
+/**
+ * Allows subscribing to an event. Takes an event name, a callback handler 
+ * invoked on each event with the specific type is emitted and an optional third argument, 
+ * which when presented serves as execution context (this) for the handler callback.
+ * @param {string} evntType event type name
+ * @param {(...args: any[]) => void} handler subscriber callback invoked on each event emitted
+ * @param {any} [thisArg] if present is used as this for the handler
+ * @returns {{ unsubscribe: () => void }} object exposing unsunscribe functionality
+ */
+function subscribe(evntType, handler, thisArg) {
+    const id = Symbol("id");
+
+    // create new event type entry if not present
+    if (!subscriptions.hasOwnProperty(evntType)) {
+        subscriptions[evntType] = {};
+    }
+
+    // register subscriber / event handler function
+    subscriptions[evntType][id] = thisArg 
+        ? handler.bind(thisArg) 
+        : handler;
+    
+    return {
+        unsubscribe: () => {
+            delete subscriptions[evntType][id];
+            if (Reflect.ownKeys(subscriptions[evntType]).length === 0) {
+                delete subscriptions[evntType];
+            }
+        }
+    }
+}
+
+/**
+ * Emits an event of certain type with arguments to be passed down to the subscribers.
+ * @param {string} evntType event type name
+ * @param  {...any} args arguments list to be passed to subscribers
+ * @returns {void}
+ */
+function publish(evntType, ...args) {
+    if (!subscriptions[evntType]) return;
+
+    Reflect.ownKeys(subscriptions[evntType])
+        .forEach(id => subscriptions[evntType][id](...args));
+}
+
+export default {
+    publish,
+    subscribe
+};
