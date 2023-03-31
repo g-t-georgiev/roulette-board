5a0bfc63d7e7490630f34198b63ab4833810bf57
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-03-31 03:02:31 +0300

    Moved utility functions into a class library.

---
+++ utils/Roulette.js
@@ -0,0 +1,29 @@
+/**
+ * Utility methods library
+ */
+export class Roulette {
+
+    constructor() {
+        if (this.constructor.name === 'Roulette') {
+            throw new Error('Roulette class is non-instantiable.');
+        }
+    }
+
+    /**
+     * Factory function for creating custom events.
+     * @param {string} evntType Event name.
+     * @param {{ bubbles?: boolean, composed?: boolean, cancelable?: boolean, detail: any }} config Init options for the custom event.
+     * @returns {CustomEvent<any>}
+     */
+    static customEvent(evntType, config = {}) {
+        return new CustomEvent(
+            evntType, 
+            {
+                bubbles: config.bubbles ?? true,
+                composed: config.bubbles ?? false,
+                cancelable: config.bubbles ?? false,
+                detail: config.detail
+            }
+        );
+    }
+}
