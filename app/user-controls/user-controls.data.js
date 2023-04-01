0bc23700495e4218b178dc3f00d39779c98f7a67
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-04-01 22:18:54 +0300

    Added chips and buttons user controls metadata in separate file.

---
+++ app/user-controls/user-controls.data.js
@@ -0,0 +1,41 @@
+export const buttons = [
+    {
+        is: 'roulette-undo-button',
+        classList: [ 'btn-undo' ],
+        imageUrl: 'btn-undo.png',
+        alt: 'Undo last bet button',
+        title: 'Undo last bet'
+    },
+    {
+        is: 'roulette-clear-button',
+        classList: [ 'btn-clear' ],
+        imageUrl: 'btn-clear.png',
+        alt: 'Clear all bets button',
+        title: 'Clear all bets'
+    },
+    {
+        is: 'roulette-double-button',
+        classList: [ 'btn-double' ],
+        imageUrl: 'btn-x2.png',
+        alt: 'Double all bets button',
+        title: 'Double all bets button'
+    }
+];
+
+export const chips = [
+    {
+        id: 1,
+        value: 1,
+        selected: false,
+    },
+    {
+        id: 2,
+        value: 2,
+        selected: true,
+    },
+    {
+        id: 3,
+        value: 5,
+        selected: false,
+    },
+];
