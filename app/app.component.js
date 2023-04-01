38d17815aac938cd1e84c606fe3159222de529ca
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-04-01 22:24:51 +0300

    Refactored: Layout content using #createElement instead of #innerHTML

--- app/app.component.js
+++ app/app.component.js
@@ -1,19 +1,18 @@
 import { Component } from '../app/core/interfaces/index.js';
 
-import createTemplate from './app.template.js';
-
 import { BoardComponent } from './board/board.component.js';
 import { UserControlsComponent } from './user-controls/user-controls.component.js';
 import { NotificationsComponent } from './notifications/notifications.component.js';
+import { ThemeSwitchComponent } from './theme-switch/theme-switch.component.js';
 
 customElements.define('roulette-board', BoardComponent);
 customElements.define('roulette-user-controls', UserControlsComponent);
 customElements.define('roulette-notifications', NotificationsComponent, { extends: 'h2' });
+customElements.define('roulette-theme-switch', ThemeSwitchComponent);
 
 export class AppComponent extends Component {
 
-    #template = document.createElement('template');
-    #shadowRoot = this.attachShadow({ mode: 'open' });
+    #shadowRoot = this.attachShadow({ mode: 'closed' });
 
     constructor() {
         super();
@@ -21,8 +20,31 @@ export class AppComponent extends Component {
     }
 
     #render() {
-        this.#template.innerHTML = createTemplate().trim();
-        this.#shadowRoot.append(this.#template.content.cloneNode(true));
+        const stylesheetElem = document.createElement('link');
+        stylesheetElem.setAttribute('rel', 'stylesheet');
+        stylesheetElem.setAttribute('href', '/app/app.component.css');
+
+        const contentWrapperElem = document.createElement('section');
+        contentWrapperElem.id = 'roulette-board-app-container';
+
+        const notificationElem = document.createElement('h2', { is: 'roulette-notifications' });
+        notificationElem.classList.add('notification');
+        notificationElem.textContent = 'Select a chip to start betting';
+
+        const rouletteBoardElem = document.createElement('roulette-board');
+        const rouletteUserControlsElem = document.createElement('roulette-user-controls');
+
+        const toggleThemeElem = document.createElement('roulette-theme-switch');
+        const isThemeDark = this.getAttribute('theme') === 'dark';
+        toggleThemeElem.toggle(isThemeDark);
+
+        contentWrapperElem.append(toggleThemeElem, notificationElem, rouletteBoardElem, rouletteUserControlsElem);
+
+        this.#shadowRoot.append(stylesheetElem, contentWrapperElem);
+
+        this.addEventListener('roulette:theme', (e) => {
+            this.setAttribute('theme', e.detail.darkMode ? 'dark' : 'light');
+        });
     }
 
     connectedCallback() {
