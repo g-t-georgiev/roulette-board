a01da5604073a180aef0f35f154dd20b3428c5bd
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-04-03 16:04:24 +0300

    Refactored data transformation code.

--- app/board/board.data.js
+++ app/board/board.data.js
@@ -1,11 +1,11 @@
 // Ranges 1-10 and 19-28 odd numbers are red, even numbers are black;
 // Ranges 11-18 and 29-36 odd numbers are black, even numbers are red;
 
-const sequence = [ 
-    0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 'col3-36', 
-    2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 'col2-35', 
-    1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34, 'col1-34', 
-    'dozen1-12', 'dozen13-24', 'dozen25-36', 
+const sequence = [
+    0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 'col3-36',
+    2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 'col2-35',
+    1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34, 'col1-34',
+    'dozen1-12', 'dozen13-24', 'dozen25-36',
     'range1-18', 'even', 'red', 'black', 'odd', 'range19-36'
 ];
 
@@ -26,97 +26,146 @@ const sequence = [
  */
 function createSlotDTO(textContent, classList) {
     return {
-        textContent, 
+        textContent,
         classList
     };
 }
 
 /**
  * @param {number | string} entry 
- * @returns {{ value: Slot, done: boolean }}
+ * @returns {Slot}
  */
-function* resolveSlotData(entry) {
+function formatData(entry) {
     if (typeof entry === 'string') {
-       if (entry === 'col3-36') {
-            yield createSlotDTO(
+
+        if (entry === 'col3-36') {
+            return createSlotDTO(
                 '2:1',
-                [ 'outside-bet', 'column', 'col-3' ]
+                ['outside-bet', 'column', 'col-3']
             );
-       } else if (entry === 'col2-35') {
-            yield createSlotDTO(
+        }
+
+        if (entry === 'col2-35') {
+            return createSlotDTO(
                 '2:1',
-                [ 'outside-bet', 'column', 'col-2' ]
+                ['outside-bet', 'column', 'col-2']
             );
-       } else if (entry === 'col1-34') {
-                yield createSlotDTO(
-                    '2:1',
-                    [ 'outside-bet', 'column', 'col-1' ]
-                );
-       } else if (entry === 'dozen1-12') {
-                yield createSlotDTO(
-                    '1st 12',
-                    [ 'outside-bet', 'dozen', 'dozen-1' ]
-                );
-       } else if (entry === 'dozen13-24') {
-                yield createSlotDTO(
-                    '2nd 12',
-                    [ 'outside-bet', 'dozen', 'dozen-2' ]
-                );
-       } else if (entry === 'dozen25-36') {
-                yield createSlotDTO(
-                    '3rd 12',
-                    [ 'outside-bet', 'dozen', 'dozen-3' ]
-                );
-       } else if (entry === 'range1-18') {
-                yield createSlotDTO(
-                    '1 - 18',
-                    [ 'outside-bet', 'range', 'range-1' ]
-                );
-       } else if (entry === 'even') {
-                yield createSlotDTO(
-                    'Even',
-                    [ 'outside-bet', 'even' ]
-                )
-       } else if (entry === 'red') {
-                yield createSlotDTO(
-                    'Red',
-                    [ 'outside-bet', 'red' ]
-                );
-       } else if (entry === 'black') {
-                yield createSlotDTO(
-                    'Black',
-                    [ 'outside-bet', 'black' ]
-                );
-       } else if (entry === 'odd') {
-                yield createSlotDTO(
-                    'Odd',
-                    [ 'outside-bet', 'odd' ]
-                )
-       } else {
-                yield createSlotDTO(
-                    '19 - 36',
-                    [ 'outside-bet', 'range', 'range-2' ]
-                );
-       }
-    } else if (entry === 0) {
-        yield createSlotDTO(entry.toString(), [ 'inside-bet', 'zero', 'even', `n-${entry}` ]);
-    } else if (
-        (entry >= 1 && entry <= 10) || 
+        }
+
+        if (entry === 'col1-34') {
+            return createSlotDTO(
+                '2:1',
+                ['outside-bet', 'column', 'col-1']
+            );
+        }
+
+        if (entry === 'dozen1-12') {
+            return createSlotDTO(
+                '1st 12',
+                ['outside-bet', 'dozen', 'dozen-1']
+            );
+        }
+
+        if (entry === 'dozen13-24') {
+            return createSlotDTO(
+                '2nd 12',
+                ['outside-bet', 'dozen', 'dozen-2']
+            );
+        }
+
+        if (entry === 'dozen25-36') {
+            return createSlotDTO(
+                '3rd 12',
+                ['outside-bet', 'dozen', 'dozen-3']
+            );
+        }
+
+        if (entry === 'range1-18') {
+            return createSlotDTO(
+                '1 - 18',
+                ['outside-bet', 'range', 'range-1']
+            );
+        }
+
+        if (entry === 'even') {
+            return createSlotDTO(
+                'Even',
+                ['outside-bet', 'even']
+            )
+        }
+
+        if (entry === 'red') {
+            return createSlotDTO(
+                'Red',
+                ['outside-bet', 'red']
+            );
+        }
+
+        if (entry === 'black') {
+            return createSlotDTO(
+                'Black',
+                ['outside-bet', 'black']
+            );
+        }
+
+        if (entry === 'odd') {
+            return createSlotDTO(
+                'Odd',
+                ['outside-bet', 'odd']
+            )
+        }
+
+        if (entry === 'range19-36') {
+            return createSlotDTO(
+                '19 - 36',
+                ['outside-bet', 'range', 'range-2']
+            );
+        }
+
+    }
+
+    if (entry === 0) {
+        return createSlotDTO(entry.toString(), ['inside-bet', 'zero', 'even', `n-${entry}`]);
+    }
+
+    const classList = [];
+
+    if (entry >= 1 && entry <= 18) {
+        if (entry <= 12) {
+            classList.push('doz-1');
+        } else {
+            classList.push('doz-2');
+        }
+
+        classList.push('rn-1');
+    }
+
+    if (entry >= 19 && entry <= 36) {
+        if (entry <= 24) {
+            classList.push('doz-2');
+        }
+
+        if (entry >= 25 && entry <= 36) {
+            classList.push('doz-3');
+        }
+
+        classList.push('rn-2');
+    }
+
+    if (
+        (entry >= 1 && entry <= 10) ||
         (entry >= 19 && entry <= 28)
     ) {
-        yield entry % 2 === 0 
-            ? createSlotDTO(entry.toString(), [ 'inside-bet', 'straight-up', 'black', 'even', `n-${entry}` ]) 
-            : createSlotDTO(entry.toString(), [ 'inside-bet', 'straight-up', 'red', 'odd', `n-${entry}` ]);
-    } else {
-        return entry % 2 === 0 
-            ? createSlotDTO(entry.toString(), [ 'inside-bet', 'straight-up', 'red', 'even', `n-${entry}` ])
-            : createSlotDTO(entry.toString(), [ 'inside-bet', 'straight-up', 'black', 'odd', `n-${entry}` ])
+        return entry % 2 === 0
+            ? createSlotDTO(entry.toString(), [ 'inside-bet', 'straight-up', 'black', 'even', `n-${entry}`, ...classList ])
+            : createSlotDTO(entry.toString(), [ 'inside-bet', 'straight-up', 'red', 'odd', `n-${entry}`, ...classList ]);
     }
+
+    return entry % 2 === 0
+        ? createSlotDTO(entry.toString(), ['inside-bet', 'straight-up', 'red', 'even', `n-${entry}`, ...classList ])
+        : createSlotDTO(entry.toString(), ['inside-bet', 'straight-up', 'black', 'odd', `n-${entry}`, ...classList ]);
 }
 
 export function getData() {
-    return sequence.map(entry => {
-        const { value, done } = resolveSlotData(entry).next();
-        return value;
-    });
+    return sequence.map(entry => formatData(entry));
 }
