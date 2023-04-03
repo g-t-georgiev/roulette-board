c481bd68480610021af5ad8bf42e11822126033a
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-04-03 20:31:24 +0300

    Deleted chips data object as its being moved into separate file, part of the chips container element directory.

--- app/user-controls/user-controls.data.js
+++ app/user-controls/user-controls.data.js
@@ -21,21 +21,3 @@ export const buttons = [
         title: 'Double all bets button'
     }
 ];
-
-export const chips = [
-    {
-        id: 1,
-        value: 1,
-        selected: false,
-    },
-    {
-        id: 2,
-        value: 2,
-        selected: true,
-    },
-    {
-        id: 3,
-        value: 5,
-        selected: false,
-    },
-];
