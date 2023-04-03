392c9461eb883a8ec84283037e730ed062eeb9d4
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-04-03 20:28:43 +0300

    Included responsive styles.

--- app/app.component.js
+++ app/app.component.js
@@ -1,11 +1,13 @@
 import { Component } from '../app/core/interfaces/index.js';
 
 import { BoardComponent } from './board/board.component.js';
+import { ChipsContainerComponent } from './chips/chips.component.js';
 import { UserControlsComponent } from './user-controls/user-controls.component.js';
 import { NotificationsComponent } from './notifications/notifications.component.js';
 import { ThemeSwitchComponent } from './theme-switch/theme-switch.component.js';
 
 customElements.define('roulette-board', BoardComponent);
+customElements.define('roulette-chips', ChipsContainerComponent);
 customElements.define('roulette-user-controls', UserControlsComponent);
 customElements.define('roulette-notifications', NotificationsComponent, { extends: 'h2' });
 customElements.define('roulette-theme-switch', ThemeSwitchComponent);
@@ -20,9 +22,17 @@ export class AppComponent extends Component {
     }
 
     #render() {
+        const stylesheets = [];
+
         const stylesheetElem = document.createElement('link');
         stylesheetElem.setAttribute('rel', 'stylesheet');
         stylesheetElem.setAttribute('href', '/app/app.component.css');
+
+        const responsiveStylesheetElem = document.createElement('link');
+        responsiveStylesheetElem.setAttribute('rel', 'stylesheet');
+        responsiveStylesheetElem.setAttribute('href', '/app/responsive.part.css');
+
+        stylesheets.push(stylesheetElem, responsiveStylesheetElem);
 
         const contentWrapperElem = document.createElement('section');
         contentWrapperElem.id = 'roulette-board-app-container';
@@ -32,15 +42,20 @@ export class AppComponent extends Component {
         notificationElem.textContent = 'Select a chip to start betting';
 
         const rouletteBoardElem = document.createElement('roulette-board');
+        const rouletteChipsContainerElem = document.createElement('roulette-chips');
         const rouletteUserControlsElem = document.createElement('roulette-user-controls');
 
         const toggleThemeElem = document.createElement('roulette-theme-switch');
         const isThemeDark = this.getAttribute('theme') === 'dark';
         toggleThemeElem.__toggle(isThemeDark);
 
-        contentWrapperElem.append(toggleThemeElem, notificationElem, rouletteBoardElem, rouletteUserControlsElem);
+        contentWrapperElem.append( 
+            rouletteBoardElem, 
+            rouletteChipsContainerElem, 
+            rouletteUserControlsElem
+        );
 
-        this.#shadowRoot.append(stylesheetElem, contentWrapperElem);
+        this.#shadowRoot.append(...stylesheets, toggleThemeElem, notificationElem, contentWrapperElem);
 
         this.addEventListener('roulette:theme', (e) => {
             this.setAttribute('theme', e.detail.darkMode ? 'dark' : 'light');
