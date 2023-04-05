d45d7fbbdf5af9c74601034d35b4fe1a4faa163c
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-04-05 15:40:36 +0300

    Removed imports, definitions and usage of notifications and theme-switch components.

--- app/app.component.js
+++ app/app.component.js
@@ -3,14 +3,10 @@ import { Component } from '../app/core/interfaces/index.js';
 import { BoardComponent } from './board/board.component.js';
 import { ChipsContainerComponent } from './chips/chips.component.js';
 import { UserControlsComponent } from './user-controls/user-controls.component.js';
-import { NotificationsComponent } from './notifications/notifications.component.js';
-import { ThemeSwitchComponent } from './theme-switch/theme-switch.component.js';
 
 customElements.define('roulette-board', BoardComponent);
 customElements.define('roulette-chips', ChipsContainerComponent);
 customElements.define('roulette-user-controls', UserControlsComponent);
-customElements.define('roulette-notifications', NotificationsComponent, { extends: 'h2' });
-customElements.define('roulette-theme-switch', ThemeSwitchComponent);
 
 export class AppComponent extends Component {
 
@@ -37,17 +33,9 @@ export class AppComponent extends Component {
         const contentWrapperElem = document.createElement('section');
         contentWrapperElem.id = 'roulette-board-app-container';
 
-        const notificationElem = document.createElement('h2', { is: 'roulette-notifications' });
-        notificationElem.classList.add('notification');
-        notificationElem.textContent = 'Select a chip to start betting';
-
         const rouletteBoardElem = document.createElement('roulette-board');
         const rouletteChipsContainerElem = document.createElement('roulette-chips');
         const rouletteUserControlsElem = document.createElement('roulette-user-controls');
-
-        const toggleThemeElem = document.createElement('roulette-theme-switch');
-        const isThemeDark = this.getAttribute('theme') === 'dark';
-        toggleThemeElem.__toggle(isThemeDark);
 
         contentWrapperElem.append( 
             rouletteBoardElem, 
@@ -56,10 +44,6 @@ export class AppComponent extends Component {
         );
 
         this.#shadowRoot.append(...stylesheets, toggleThemeElem, notificationElem, contentWrapperElem);
-
-        this.addEventListener('roulette:theme', (e) => {
-            this.setAttribute('theme', e.detail.darkMode ? 'dark' : 'light');
-        });
     }
 
     connectedCallback() {
