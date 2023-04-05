87d9f23b4bb643e4b548bdb880349ad3c9f4a998
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-04-05 17:17:53 +0300

    Created separate slot chip component with its own styles and behaviour.

---
+++ app/board/slot/slot-chip/slot-chip.component.js
@@ -0,0 +1,55 @@
+import { Component } from '../../../core/interfaces/index.js';
+
+export class SlotChipComponent extends Component {
+
+    #shadowRoot = this.attachShadow({ mode: 'open' });
+
+    constructor() {
+        super();
+        this.rendered = false;
+    }
+
+    get textContent() {
+        return this.#shadowRoot.querySelector('.chip').textContent;
+    }
+
+    set textContent(v) {
+        this.#shadowRoot.querySelector('.chip').textContent = v;
+    }
+
+    #render() {
+        const stylesheets = [];
+
+        const stylesheetElem = document.createElement('link');
+        stylesheetElem.rel = 'stylesheet';
+        stylesheetElem.href = '/app/board/slot/slot-chip/slot-chip.component.css';
+
+        stylesheets.push(stylesheetElem);
+
+        const div = document.createElement('div');
+
+        div.classList.add('chip');
+        div.textContent = this.dataset.value;
+
+        const img = document.createElement('img');
+        img.classList.add('chip-icon');
+        img.setAttribute('src', `/assets/images/chip-background-${this.dataset.id}.png`);
+        img.setAttribute('alt', 'chip icon');
+        img.setAttribute('width', '30');
+        img.setAttribute('height', '30');
+
+        div.append(img);
+
+        this.#shadowRoot.append(...stylesheets, div);
+    }
+
+    connectedCallback() {
+        if (!this.rendered) {
+            this.rendered = true;
+            this.#render();
+        }
+    }
+
+    disconnectedCallback() {}
+
+}
