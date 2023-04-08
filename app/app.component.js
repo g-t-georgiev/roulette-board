e5376b4cb37a866c2afeb9dc92f8a5c88f874b33
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-04-08 18:09:05 +0300

    Imported and defined popup component on global (app) level.

--- app/app.component.js
+++ app/app.component.js
@@ -3,10 +3,12 @@ import { Component } from '../app/core/interfaces/index.js';
 import { BoardComponent } from './board/board.component.js';
 import { ChipsContainerComponent } from './chips/chips.component.js';
 import { UserControlsComponent } from './user-controls/user-controls.component.js';
+import { PopUpComponent } from './pop-up/pop-up.component.js';
 
 customElements.define('roulette-board', BoardComponent);
 customElements.define('roulette-chips', ChipsContainerComponent);
 customElements.define('roulette-user-controls', UserControlsComponent);
+customElements.define('roulette-popup', PopUpComponent);
 
 export class AppComponent extends Component {
 
