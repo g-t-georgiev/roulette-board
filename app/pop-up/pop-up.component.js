b2c9321147814956784f9d5dee57e57522966da5
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-04-08 20:30:10 +0300

    Refactored #render as async method; In initializer method assign #onRenderCAllback a function which is invoked in the #render method and thus preventing null pointer exceptions.

--- app/pop-up/pop-up.component.js
+++ app/pop-up/pop-up.component.js
@@ -8,6 +8,7 @@ export class PopUpComponent extends Component {
     #shadowRoot = this.attachShadow({ mode: 'open' });
     #textElem;
     #timerId;
+    #onRenderCallback = () => {};
 
     constructor() {
         super();
@@ -16,50 +17,50 @@ export class PopUpComponent extends Component {
 
     initialize(data) {
         // console.log(data);
-        this.#textElem.classList.toggle('chip-added', data.state === 'appended');
-        this.#textElem.classList.toggle('chip-removed', data.state === 'removed');
-        // console.log(this.#textContent.classList);
 
-        const sign = data.state === 'appended' ? '+' : '-';
-        
-        this.#textElem.textContent = sign + (typeof data.value === 'number' ? data.value : Number(data.value)).toFixed(2);
+        this.#onRenderCallback = (textElem) => {
+            // console.log(textElem);
+            if (!textElem) return;
 
-        this.#timerId = setTimeout(() => {
-            this?.remove();
-        }, 1e3);
+            textElem.classList.toggle('chip-added', data.state === 'appended');
+            textElem.classList.toggle('chip-removed', data.state === 'removed');
+
+            const sign = data.state === 'appended' ? '+' : '-';
+            
+            textElem.textContent = sign + (typeof data.value === 'number' ? data.value : Number(data.value)).toFixed(2);
+
+            this.#timerId = setTimeout(() => {
+                this?.remove();
+            }, 1e3);
+        }
     }
 
-    #render() {
-
-        if (!stylesheets.length) {
-
-            Roulette.fetchComponentStyles('/app/pop-up/pop-up.component.css')
-                .then(cssText => {
-                    // console.log(cssText);
-                    const styleElem = document.createElement('style');
-                    styleElem.textContent = cssText;
-                    // console.log(styleElem);
-                    stylesheets.push(styleElem);
-                    this.#shadowRoot.prepend(styleElem);
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-
-        } else {
-            this.#shadowRoot.prepend(...stylesheets.map(styleEl => styleEl.cloneNode(true)));
+    async #render() {
+        try {
+            if (!stylesheets.length) {
+                const cssText = await Roulette.fetchComponentStyles('/app/pop-up/pop-up.component.css');
+                // console.log(cssText);
+                const styleElem = document.createElement('style');
+                styleElem.textContent = cssText;
+                // console.log(styleElem);
+                stylesheets.push(styleElem);
+                this.#shadowRoot.prepend(styleElem);
+            } else {
+                this.#shadowRoot.prepend(...stylesheets.map(styleEl => styleEl.cloneNode(true)));
+            }
+    
+            const notificationElem = document.createElement('div');
+            notificationElem.classList.add('notification');
+    
+            this.#textElem = document.createElement('span');
+            this.#textElem.classList.add('notification-text');
+            this.#onRenderCallback?.(this.#textElem);
+            
+            notificationElem.append(this.#textElem);
+            this.#shadowRoot.append(notificationElem);
+        } catch (error) {
+            console.error(error);
         }
-
-        const notificationElem = document.createElement('div');
-        notificationElem.classList.add('notification');
-
-        this.#textElem = document.createElement('span');
-        this.#textElem.classList.add('notification-text');
-
-        notificationElem.append(this.#textElem);
-
-        this.#shadowRoot.append(notificationElem);
-
     }
 
     connectedCallback() {
