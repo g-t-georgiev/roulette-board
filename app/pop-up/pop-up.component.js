99e80eb3110e6c7e47d025b766dc283e13148ec2
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-04-08 18:17:17 +0300

    Changed initial text element holdind reference property; added initializer method and disconnectedCallback logic cancelling timeouts.

--- app/pop-up/pop-up.component.js
+++ app/pop-up/pop-up.component.js
@@ -6,11 +6,27 @@ const stylesheets = [];
 export class PopUpComponent extends Component {
 
     #shadowRoot = this.attachShadow({ mode: 'open' });
-    #textContent;
+    #textElem;
+    #timerId;
 
     constructor() {
         super();
         this.rendered = false;
+    }
+
+    initialize(data) {
+        // console.log(data);
+        this.#textElem.classList.toggle('chip-added', data.state === 'appended');
+        this.#textElem.classList.toggle('chip-removed', data.state === 'removed');
+        // console.log(this.#textContent.classList);
+
+        const sign = data.state === 'appended' ? '+' : '-';
+        
+        this.#textElem.textContent = sign + (typeof data.value === 'number' ? data.value : Number(data.value)).toFixed(2);
+
+        this.#timerId = setTimeout(() => {
+            this?.remove();
+        }, 1e3);
     }
 
     #render() {
@@ -37,12 +53,12 @@ export class PopUpComponent extends Component {
         const notificationElem = document.createElement('div');
         notificationElem.classList.add('notification');
 
-        this.#textContent = document.createElement('span');
-        this.#textContent.classList.add('notification-text');
+        this.#textElem = document.createElement('span');
+        this.#textElem.classList.add('notification-text');
 
-        notificationElem.append(this.#textContent);
+        notificationElem.append(this.#textElem);
 
-        this.#shadowRoot.append(this.#textContent);
+        this.#shadowRoot.append(notificationElem);
 
     }
 
@@ -52,4 +68,11 @@ export class PopUpComponent extends Component {
             this.#render();
         }
     }
+
+    disconnectedCallback() {
+        // console.log('Popup component removed.');
+        // console.log(this.#timerId);
+        clearTimeout(this.#timerId);
+    }
+
 }
