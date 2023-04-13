5f25b7234ae7631eb2f4551fa2a405d55e9a6e3c
g-t-georgiev <georgi.toni.georgiev@gmail.com>
2023-04-13 21:49:16 +0300

    Refactored component base class module.

---
+++ app/core/interfaces/component/component.js
@@ -0,0 +1,48 @@
+/**
+ * Component abstract class.
+ */
+export class Component extends HTMLElement {
+
+    constructor() {
+        super();
+        
+        if (this.constructor.name === 'Component') {
+            throw new Error('Component is an abstract class.');
+        }
+    }
+
+    /**
+     * This method is called when the custom 
+     * element is rendered into the DOM. This is 
+     * useful for making initial API calls for fetching data, etc.
+     */
+    connectedCallback() { }
+
+    /**
+     * This method is called when the custom 
+     * element is removed from the DOM. This is
+     * useful for clean-up, such as removing event 
+     * listeners, unsubscribing from observables, etc.
+     */
+    disconnectedCallback() { }
+
+    /**
+     * This is a static getter property that should 
+     * return an array of the names of attributes you 
+     * wish to observe for changes.
+     * @returns {Array<string>}
+     */
+    static get observedAttributes() { }
+
+    /**
+     * This method is called each time one of the observed 
+     * attributes changes, including after the element has been
+     * rendered into the DOM. The first argument is the attribute name, 
+     * followed by the previous and current values. 
+     * @param {string} attribute 
+     * @param {string} previousValue 
+     * @param {string} currentValue 
+     */
+    attributeChangedCallback(attribute, previousValue, currentValue) { }
+
+}
