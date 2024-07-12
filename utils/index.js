2f3ffec8d330821934bbdba0e1b088c96f1c3d89
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2024-07-12 18:04:01 +0300

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -1,2 +1,2 @@
-export { PubSub } from '../node_modules/@g.t.georgiev/pubsub/src/PubSub.js';
-export { forEachOf } from '../node_modules/@g.t.georgiev/foreachof/src/index.js';
+export { PubSub } from 'https://unpkg.com/@g.t.georgiev/pubsub@2.1.2/src/PubSub.js';
+export { forEachOf } from 'https://unpkg.com/@g.t.georgiev/foreachof@1.1.0/src/index.js';
