0ef6c320f039b66442c155d3aa30671359102bbc
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2024-07-14 11:09:39 +0000

    Removed unnecessary binded method version for the selected chip event handler by refactoring the private method into a private field assign with an arrow function in the constructor.

--- app/chips/chips.component.js
+++ app/chips/chips.component.js
@@ -6,11 +6,37 @@ customElements.define('roulette-chip', ChipComponent);
 
 export class ChipsContainerComponent extends Component {
 
+    #selectHandler;
     #shadowRoot = this.attachShadow({ mode: 'open' });
 
     constructor() {
         super();
         this.rendered = false;
+
+        /**
+         * Manage custom events about selected chip.
+         * @param {CustomEvent<{ id: string, value: string, selected: boolean }>} e 
+         */
+        this.#selectHandler = (e) => {
+            // console.log(e.detail);
+    
+            const chips = this.#shadowRoot.querySelectorAll('roulette-chip[selected]');
+            // console.log(chips);
+    
+            /**
+             * Callback looping over chip instances with selected attribute.
+             * Calls #toggleSelectState method if a possible previously selected chip instance
+             * is not the trigger of the current event.
+             * @param {ChipComponent} chip 
+             */
+            const cb = chip => {
+                const isEvntTarget = chip.dataset.id === e.detail.id;
+                const isSelected = isEvntTarget && e.detail.selected;
+                chip.selected = isSelected;
+            };
+    
+            chips.forEach(cb);
+        };
     }
 
     #render() {
@@ -34,43 +60,8 @@ export class ChipsContainerComponent extends Component {
             return elem;
         });
 
-        this.addEventListener('roulette:chipselected', this.__selectHandler);
+        this.addEventListener('roulette:chipselected', this.#selectHandler);
         this.#shadowRoot.append(...stylesheets, ...chipElemList);
-    }
-
-    /**
-     * Manage custom events about selected chip.
-     * @param {CustomEvent<{ id: string, value: string, selected: boolean }>} e 
-     */
-    #selectHandler(e) {
-        // console.log(e.detail);
-
-        const chips = this.#shadowRoot.querySelectorAll('roulette-chip[selected]');
-        // console.log(chips);
-
-        /**
-         * Callback looping over chip instances with selected attribute.
-         * Calls #toggleSelectState method if a possible previously selected chip instance
-         * is not the trigger of the current event.
-         * @param {ChipComponent} chip 
-         */
-        const cb = chip => {
-            const isEvntTarget = chip.dataset.id === e.detail.id;
-            const isSelected = isEvntTarget && e.detail.selected;
-            chip.selected = isSelected;
-        };
-
-        chips.forEach(cb);
-    }
-
-    /**
-     * Select chip event handler method for internal use only.
-     * Forwards calls to its private counterpart, binding execution 
-     * context to the component instance.
-     * @param  {...any} args 
-     */
-    __selectHandler(...args) {
-        this.#selectHandler.call(this, ...args);
     }
 
     connectedCallback() {
@@ -81,7 +72,7 @@ export class ChipsContainerComponent extends Component {
     }
 
     disconnectedCallback() {
-        this.removeEventListener('roulette:chipselected', this.__selectHandler);
+        this.removeEventListener('roulette:chipselected', this.#selectHandler);
     }
 
 }
